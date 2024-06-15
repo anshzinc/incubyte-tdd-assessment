ee6c7ab833c35c927363cf495f514da14e897639
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:02:20 +0530

    Add test cases for string of numbers

--- add.spec.ts
+++ add.spec.ts
@@ -3,3 +3,8 @@ import add  from './add';
  test('should handle empty string', () => {
     expect(add("")).toBe(0);
  });
+ 
+ test('should handle string of comma separated numbers', () => {
+   expect(add("1")).toBe(1); 
+   expect(add("1,5")).toBe(6);
+ });
