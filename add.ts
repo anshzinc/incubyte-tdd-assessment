d0ff2e2b55228454dfd730f5189c9f605c40819c
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:30:54 +0530

    Handle custom delimiters

--- add.ts
+++ add.ts
@@ -1,9 +1,14 @@
 const add = (numbers: string) => {
   if (numbers === "") return 0;
+  // Check if string has custom delimiter
+  let delimiter = ",";
+  if (numbers.indexOf("//") === 0 && numbers.indexOf("\n") === 3) {
+    delimiter = numbers[2];
+  }
   let sum = 0;
   const nums = numbers
     .replace(/\n/g, ",") // treat newlines as default delimiter (comma)
-    .split(",")
+    .split(delimiter)
     .map((item) => (!isNaN(Number(item)) ? Number(item) : 0));
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
