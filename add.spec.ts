ca1393435183bbd2d040c4b26381d69e9843c8bf
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:25:04 +0530

    Add basic test case for custom delimiters

--- add.spec.ts
+++ add.spec.ts
@@ -1,26 +1,30 @@
-import add  from './add';
+import add from "./add";
 
- test('should handle empty string', () => {
-    expect(add("")).toBe(0);
- });
- 
- test('should handle string of comma separated numbers', () => {
-   expect(add("1")).toBe(1); 
-   expect(add("1,5")).toBe(6);
- });
- 
- test('should handle any amount of integers', () => {
-   expect(add("1,0,1,0,2,3,4,5,6,7,10")).toBe(39); 
- });
- 
- test('should handle multiple subsequent commans', () => {
-   expect(add("1,0,1,0,2,,,3,4,5,6,7,,10,")).toBe(39); 
- });
- 
- test('should handle new lines between numbers', () => {
-   expect(add("1,0,1,0,2,,,3,4,5,6\n7,,10,")).toBe(39); 
-   expect(add("1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10,")).toBe(39); 
-   expect(add("\n1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n")).toBe(39); 
-   expect(add("\n1,0,1,0,,,3\n\n\n4,5,6\n7,,10,")).toBe(37); 
-   expect(add("0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n,\n\n,,")).toBe(38); 
- });
+test("should handle empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test("should handle string of comma separated numbers", () => {
+  expect(add("1")).toBe(1);
+  expect(add("1,5")).toBe(6);
+});
+
+test("should handle any amount of integers", () => {
+  expect(add("1,0,1,0,2,3,4,5,6,7,10")).toBe(39);
+});
+
+test("should handle multiple subsequent commans", () => {
+  expect(add("1,0,1,0,2,,,3,4,5,6,7,,10,")).toBe(39);
+});
+
+test("should handle new lines between numbers", () => {
+  expect(add("1,0,1,0,2,,,3,4,5,6\n7,,10,")).toBe(39);
+  expect(add("1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10,")).toBe(39);
+  expect(add("\n1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n")).toBe(39);
+  expect(add("\n1,0,1,0,,,3\n\n\n4,5,6\n7,,10,")).toBe(37);
+  expect(add("0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n,\n\n,,")).toBe(38);
+});
+
+test("should handle custom delimers at the beginning of string", () => {
+  expect(add("//;\n1;2")).toBe(2);
+});
