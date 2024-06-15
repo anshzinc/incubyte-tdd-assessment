78f97768a72a83e5399038544c826ad2eff1921c
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:16:36 +0530

    Add VSCode config to format on save

--- add.ts
+++ add.ts
@@ -1,11 +1,11 @@
 const add = (numbers: string) => {
   if (numbers === "") return 0;
   let sum = 0;
-  const nums = numbers.split(',').map(((item) => Number(item)));
-  for (let i=0; i<nums.length; i++) {
+  const nums = numbers.split(",").map((item) => Number(item));
+  for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
   }
-  return sum; 
+  return sum;
 };
 
 export default add;
