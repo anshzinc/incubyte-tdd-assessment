09f325b966d9d6dab7fea299dcd4d151fdf8a33a
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:21:56 +0530

    Update add func to handle newlines

--- add.ts
+++ add.ts
@@ -1,7 +1,10 @@
 const add = (numbers: string) => {
   if (numbers === "") return 0;
   let sum = 0;
-  const nums = numbers.split(",").map((item) => Number(item));
+  const nums = numbers
+    .replace(/\n/g, ",") // treat newlines as default delimiter (comma)
+    .split(",")
+    .map((item) => (!isNaN(Number(item)) ? Number(item) : 0));
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
   }
