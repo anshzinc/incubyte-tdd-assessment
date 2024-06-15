f7b7025034e1d0a0db89c81d417f1cf536c70152
Anshul Vyas <anshzinc@gmail.com>
2024-06-15 19:15:15 +0530

    Add test cases for new lines between numbers

--- add.spec.ts
+++ add.spec.ts
@@ -14,5 +14,13 @@ import add  from './add';
  });
  
  test('should handle multiple subsequent commans', () => {
-   expect(add("1,0,1,0,2,,,3,4,5,6,7,,10")).toBe(39); 
+   expect(add("1,0,1,0,2,,,3,4,5,6,7,,10,")).toBe(39); 
+ });
+ 
+ test('should handle new lines between numbers', () => {
+   expect(add("1,0,1,0,2,,,3,4,5,6\n7,,10,")).toBe(39); 
+   expect(add("1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10,")).toBe(39); 
+   expect(add("\n1,0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n")).toBe(39); 
+   expect(add("\n1,0,1,0,,,3\n\n\n4,5,6\n7,,10,")).toBe(37); 
+   expect(add("0,1,0,2,,,3\n\n\n4,5,6\n7,,10\n,\n\n,,")).toBe(38); 
  });
